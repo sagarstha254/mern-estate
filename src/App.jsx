61309f75adb1f900d37ba164d4d7c537b44df5cf
sagarstha254 <sagarstha254@gmail.com>
2023-12-05 06:31:16 +0545

    chore: create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signin from "./pages/Signin";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <h1 className='text-red-1000'>App</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
