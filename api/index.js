a93e235e045aabe427dffdf3fb29bff4d4e3a352
sagarstha254 <sagarstha254@gmail.com>
2023-12-06 22:55:04 +0545

    feat(databasee): database connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("connected to Database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
